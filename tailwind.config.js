9b5ff76e29955a18a6528f439a68745f99d34d0f
RU063 <rashedinislam.06@gmail.com>
2024-09-01 14:55:21 +0600

    custom font and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      rubic: "'Rubik 80s Fade', system-ui",
+      suse: "'SUSE', cursive",
+      'm-plus': "'M PLUS 1 Code', monospace"
+    },
+    extend: {
+      colors: {
+        'light-ash': '#EAEAEA',
+        'midnight-indigo': '#111827',
+        'golden-saffron': '#b97603',
+        'royal-amethyst': '#7e23a5',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
